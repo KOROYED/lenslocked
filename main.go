dcbce55fff68b45a97683c7342979b2bec8ffe5f
KOROYED <56847961+KOROYED@users.noreply.github.com>
2023-07-04 13:55:00 +0300

    staring using chi to route our app

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,18 +18,6 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	email me at <a href=\"mailto:jon@calhoun.io\>jon@calhoun.io</a>.`)
 }
 
-// func faqHandler(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-// 	fmt.Fprint(w, ` <h1>FAQ Page</h1>
-// 				    <p>Q: Is there a free version?
-// 					<p>A: Yes! We offer a free trial for 30 days on any paid plans.
-// 					<p>Q: What are your support hours?
-// 					<p>A: We have support sraff answering emails 24/7, though response times
-// 						  may be a bit slower on weekends.
-// 					<p>Q: How do i contact support?
-// 					<p>A: Email us - <a href=\"mailto:support@lenslocked.com\>support@lenslocked.com</a>.`)
-// }
-
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, ` <h1>FAQ Page</h1>
@@ -40,35 +30,15 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 </ul>`)
 }
 
-// func pathHandler(w http.ResponseWriter, r *http.Request) {
-// 	switch r.URL.Path {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default:
-// 		http.Error(w, "Page not found", http.StatusNotFound)
-// 	}
-// }
-
-type Router struct{}
-
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
 func main() {
-	var router Router
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+
 	fmt.Println("Starting the server on :3000...")
-	//http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
