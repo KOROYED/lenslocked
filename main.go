1e267298f5c1420b10b76a5c3e44214eee108117
KOROYED <56847961+KOROYED@users.noreply.github.com>
2023-07-04 12:27:51 +0300

    Added faq page

--- main.go
+++ main.go
@@ -12,7 +12,32 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.")
+	fmt.Fprint(w, `<h1>Contact Page</h1><p>To get in touch, 
+	email me at <a href=\"mailto:jon@calhoun.io\>jon@calhoun.io</a>.`)
+}
+
+// func faqHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	fmt.Fprint(w, ` <h1>FAQ Page</h1>
+// 				    <p>Q: Is there a free version?
+// 					<p>A: Yes! We offer a free trial for 30 days on any paid plans.
+// 					<p>Q: What are your support hours?
+// 					<p>A: We have support sraff answering emails 24/7, though response times
+// 						  may be a bit slower on weekends.
+// 					<p>Q: How do i contact support?
+// 					<p>A: Email us - <a href=\"mailto:support@lenslocked.com\>support@lenslocked.com</a>.`)
+// }
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, ` <h1>FAQ Page</h1>
+<ul>
+	<li><b>Is there a free version?</b>Yes! We offer a free trial for 30 days on any paid plans.</li>
+	<li><b>What are your support hours?</b>We have support sraff answering emails 24/7, though response times 
+	may be a bit slower on weekends.</li>
+	<li><b>How do i contact support?</b>Email us - 
+	<a href="mailto:support@lenslocked.com">support@lenslocked.com</a>.</li>
+</ul>`)
 }
 
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +59,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
@@ -42,5 +69,6 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var router Router
 	fmt.Println("Starting the server on :3000...")
+	//http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
 	http.ListenAndServe(":3000", router)
 }
