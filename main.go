d59389c42dfc1c0cef8d98fe2d38226bb5d294a5
Andrew <koroyed3883@gmail.com>
2024-01-18 03:11:19 +0200

    Created sign in page

--- main.go
+++ main.go
@@ -48,8 +48,13 @@ func main() {
 		templates.FS,
 		"signup.gohtml", "tailwind.gohtml",
 	))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml", "tailwind.gohtml",
+	))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.Get("/amogus", controllers.StaticHandler(views.Must(views.ParseFS(
 		templates.FS,
