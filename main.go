69f19b6584c2d94f6acec242d86cf25c35513cf8
Andrew <koroyed3883@gmail.com>
2024-01-18 03:31:00 +0200

    Implemented user authentication

--- main.go
+++ main.go
@@ -55,6 +55,7 @@ func main() {
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	r.Get("/amogus", controllers.StaticHandler(views.Must(views.ParseFS(
 		templates.FS,
