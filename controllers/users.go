22e6ddd683a69a68d386dab3898f2f350e6e1cb8
Andrew <koroyed3883@gmail.com>
2024-02-06 04:54:20 +0200

    added func for custom template

--- controllers/users.go
+++ controllers/users.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 
 	"github.com/KOROYED/lenslocked/models"
-	"github.com/gorilla/csrf"
 )
 
 type Users struct {
@@ -19,11 +17,9 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	var data struct {
-		Email     string
-		CSRFField template.HTML
+		Email string
 	}
 	data.Email = r.FormValue("email")
-	data.CSRFField = csrf.TemplateField(r)
 	u.Templates.New.Execute(w, data)
 }
 
